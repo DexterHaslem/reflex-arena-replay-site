c960a1cff6a6c4b821401a9c97dc6dc12ec75028
Dexter Haslem <dexter.haslem@gmail.com>
2016-07-20 19:01:49 -0600

    start of proper backend

---
+++ backend/src/main/java/com/dmh/reflex/ReplaysApplication.java
@@ -0,0 +1,12 @@
+package com.dmh.reflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplaysApplication.class, args);
+	}
+}
