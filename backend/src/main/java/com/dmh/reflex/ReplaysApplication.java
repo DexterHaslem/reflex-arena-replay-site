b79786a6f020b2292e537a636825f52f5d440f3e
Dexter Haslem <dexter.haslem@gmail.com>
2016-07-22 00:45:11 -0600

    fix war deployment in tomcat

--- backend/src/main/java/com/dmh/reflex/ReplaysApplication.java
+++ backend/src/main/java/com/dmh/reflex/ReplaysApplication.java
@@ -2,11 +2,19 @@ package com.dmh.reflex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReplaysApplication {
+public class ReplaysApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReplaysApplication.class, args);
+	}
+
+	// Used when deploying to a standalone servlet container
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReplaysApplication.class);
 	}
 }
